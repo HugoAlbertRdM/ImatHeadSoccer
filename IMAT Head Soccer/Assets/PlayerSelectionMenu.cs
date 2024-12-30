655dedfe6fd733155db5ffd6c7894e5f1ace7de4
mariaascaniom <mariaascaniom@gmail.com>
2024-12-30 18:44:34 +0100

    escena para seleccionar jugadores

---
+++ IMAT Head Soccer/Assets/PlayerSelectionMenu.cs
@@ -0,0 +1,67 @@
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+public class CharacterSelectionMenu : MonoBehaviour
+{
+    private int index;
+
+    [SerializeField] private Image image;
+    [SerializeField] private TextMeshProUGUI nameText;
+    private GameManager gameManager;
+
+    private void Start()
+    {
+        gameManager = GameManager.Instance;
+
+        index = PlayerPrefs.GetInt("PlayerIndex");
+
+        if (index > gameManager.players.Count - 1)
+        {
+            index = 0;
+        }
+
+        ChangeScreen();
+    }
+
+    private void ChangeScreen()
+    {
+        PlayerPrefs.SetInt("PlayerIndex", index);
+        image.sprite = gameManager.players[index].image;
+        nameText.text = gameManager.players[index].name;
+    }
+
+    public void NextCharacter()
+    {
+        if (index == gameManager.players.Count - 1)
+        {
+            index = 0;
+        }
+        else
+        {
+            index += 1;
+        }
+
+        ChangeScreen();
+    }
+
+    public void PreviousCharacter()
+    {
+        if (index == 0)
+        {
+            index = gameManager.players.Count - 1;
+        }
+        else
+        {
+            index -= 1;
+        }
+
+        ChangeScreen();
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
---
+++ IMAT Head Soccer/Assets/Scripts/Players.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName ="NewPlayer", menuName = "Player")]
+
+public class Players : ScriptableObject
+{
+    public GameObject footballPlayer;
+    public Sprite image;
+    public string name;
+    
+
+}
